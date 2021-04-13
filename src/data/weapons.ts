f3f42407cea55cfa1056a604d96ac06d302ee4da
Maciej aXenDev Balcerzak <ithereplay@gmail.com>
2021-04-13 19:19:51 +0200

    Add moduleResolution, add tippy.js

--- src/data/weapons.ts
+++ src/data/weapons.ts
@@ -1,37 +1,4 @@
-import { weaponsType } from './types/dataTypes';
-
-export const legends = [
-  'bangalore',
-  'bloodhound',
-  'caustic',
-  'crypto',
-  'fuse',
-  'gibraltar',
-  'horizon',
-  'lifeline',
-  'loba',
-  'mirage',
-  'octane',
-  'pathfinder',
-  'rampart',
-  'revenant',
-  'wattson',
-  'wraith'
-];
-
-export const bodyShields = [
-  'body_shield_1',
-  'body_shield_2',
-  'body_shield_3',
-  'body_shield_4',
-  'evo_shield_1',
-  'evo_shield_2',
-  'evo_shield_3',
-  'evo_shield_5'
-];
-
-export const helmets = ['helmet_1', 'helmet_2', 'helmet_3', 'helmet_4'];
-export const backpacks = ['backpack_1', 'backpack_2', 'backpack_3', 'backpack_4'];
+import { weaponsType } from '../types/dataTypes';
 
 // In production
 export const weapons: weaponsType = [
--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,11 @@
 import './styles/style.scss';
+import 'tippy.js/dist/tippy.css';
 import { getSettings } from './querySelectors';
 import getRandomItems from './functions/getRandomItems';
-import { bodyShields, helmets, legends, backpacks, weapons } from './data';
+import { bodyShields, helmets, legends, backpacks } from './data/data';
 import getRandomWeapon from './functions/getRandomWeapon';
+import tippy from 'tippy.js';
+import { weapons } from './data/weapons';
 
 getSettings?.addEventListener('submit', e => {
   e.preventDefault();
@@ -13,4 +16,8 @@ getSettings?.addEventListener('submit', e => {
   console.log(getRandomItems(backpacks));
 
   console.log(getRandomWeapon(weapons));
+});
+
+tippy('#bangalore_tooltip', {
+  content: 'Bangalore'
 });
