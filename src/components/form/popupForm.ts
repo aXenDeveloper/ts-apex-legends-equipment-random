176d3d0d0a15f117df99a265242aa5210586633c
Maciej aXenDev Balcerzak <ithereplay@gmail.com>
2021-04-18 22:32:05 +0200

    Add weapons in popup

--- src/components/form/popupForm.ts
+++ src/components/form/popupForm.ts
@@ -1,5 +1,11 @@
+import { convertToImgUpgrades } from './../../data/upgrades';
 import { weaponsDataName } from './../../data/weapons';
-import { backpacksDataName, helmetsDataName } from './../../data/data';
+import {
+  backpacksDataName,
+  convertToImgShields,
+  helmetsDataName,
+  convertToImgBackpacks
+} from './../../data/data';
 import { randomElementsType } from './../../types/componentsType';
 import { legendsDataName } from './../../data/legends';
 import popup from '../popup/popup';
@@ -16,32 +22,23 @@ const popupForm = (randomElements: randomElementsType) => {
     weapon2: weaponsDataName[randomElements.weapon2.name]
   };
 
-  const convertToImgShields: { [key: string]: string } = {
-    body_shield_1: 'body',
-    body_shield_2: 'body',
-    body_shield_3: 'body',
-    body_shield_4: 'body',
-    evo_shield_1: 'evo',
-    evo_shield_2: 'evo',
-    evo_shield_3: 'evo',
-    evo_shield_5: 'evo'
+  const weaponUpgrade = (array: string[]) => {
+    return array
+      .map(el => {
+        return `<li class='${el}'><img src="./images/weapons/upgrades/${convertToImgUpgrades[el]}.png" alt="${el}" /></li>`;
+      })
+      .join('');
   };
 
-  const convertToImgBackpacks: { [key: string]: string } = {
-    backpack_1: 'backpack_1',
-    backpack_2: 'backpack_2',
-    backpack_3: 'backpack_3',
-    backpack_4: 'backpack_3'
-  };
+  const weapon1Upgrades = weaponUpgrade(randomElements.weapon1.upgrades);
+  const weapon2Upgrades = weaponUpgrade(randomElements.weapon2.upgrades);
 
   const popupSchema = `
   <div class='result'>
     <div class='result_legend'>
       <img src='./images/legends/${randomElements.legend}.png' alt='${randomElementsName.legend}'>
-    </div>
 
-    <div class='result_equipment'>
-      <ul class='result_equipment_other'>
+      <ul class='result_legend_other'>
         <li class='${randomElements.bodyShield}'>
           <img src='./images/shields/${convertToImgShields[randomElements.bodyShield]}.png' alt='${
     randomElementsName.bodyShield
@@ -59,11 +56,46 @@ const popupForm = (randomElements: randomElementsType) => {
         </li>
       </ul>
     </div>
+
+
+      <ul class='result_equipment'>
+          <li class='result_equipment_item backgroundColor_energy'>
+            <h4>${weaponsDataName[randomElements.weapon1.name]}</h4>
+
+            <div class='result_equipment_item_img'>
+              <img src='./images/weapons/${randomElements.weapon1.name}.png' alt='${
+    weaponsDataName[randomElements.weapon1.name]
+  }'>
+            </div>
+            
+            ${
+              weapon1Upgrades
+                ? `<ul class='result_equipment_item_upgrades'>${weapon1Upgrades}</ul>`
+                : ''
+            }
+          </li>
+
+          <li class='result_equipment_item backgroundColor_energy'>
+            <h4>${weaponsDataName[randomElements.weapon2.name]}</h4>
+
+            <div class='result_equipment_item_img'>
+              <img src='./images/weapons/${randomElements.weapon2.name}.png' alt='${
+    weaponsDataName[randomElements.weapon2.name]
+  }'>
+            </div>
+            
+            ${
+              weapon2Upgrades
+                ? `<ul class='result_equipment_item_upgrades'>${weapon2Upgrades}</ul>`
+                : ''
+            }
+          </li>
+      </ul>
+    
   </div>
   `;
 
   popup(randomElementsName.legend, popupSchema);
-
   tooltip();
 };
 
--- src/data/data.ts
+++ src/data/data.ts
@@ -20,6 +20,16 @@ export const bodyShieldsDataName: dataNameType = {
   evo_shield_3: 'Evo Shield lvl 3',
   evo_shield_5: 'Evo Shield lvl 5'
 };
+export const convertToImgShields: dataNameType = {
+  body_shield_1: 'body',
+  body_shield_2: 'body',
+  body_shield_3: 'body',
+  body_shield_4: 'body',
+  evo_shield_1: 'evo',
+  evo_shield_2: 'evo',
+  evo_shield_3: 'evo',
+  evo_shield_5: 'evo'
+};
 
 export const helmetsData = ['helmet_1', 'helmet_2', 'helmet_3', 'helmet_4'];
 export const helmetsDataName: dataNameType = {
@@ -35,4 +45,10 @@ export const backpacksDataName: dataNameType = {
   backpack_2: 'Backpack lvl 2',
   backpack_3: 'Backpack lvl 3',
   backpack_4: 'Backpack lvl 4'
+};
+export const convertToImgBackpacks: dataNameType = {
+  backpack_1: 'backpack_1',
+  backpack_2: 'backpack_2',
+  backpack_3: 'backpack_3',
+  backpack_4: 'backpack_3'
 };
